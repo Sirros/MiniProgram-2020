3907813eb8e64d306b4d1dd3b17332385e9b4557
bales <121970263@qq.com>
2020-06-13 02:03:05 +0800

    update

--- pages/analysis_result/analysis_result.js
+++ pages/analysis_result/analysis_result.js
@@ -4,7 +4,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    result:''
+    result:'',
+    type: ''
   },
 
   /**
@@ -12,8 +13,32 @@ Page({
    */
   onLoad: function (options) {
     let temp = JSON.parse(options.result)
-    this.setData({
-      result: temp
+    console.log(temp)
+    temp.forEach(item => {
+      let that = this
+      if(item.caution === 'high') {
+        that.setData({
+          type: 'high'
+        })
+        console.log(that.type)
+        item.caution = '高'
+      } else if(item.caution === 'normal') {
+        that.setData({
+          type: 'normal'
+        })
+        console.log(that.type)
+        item.caution = '正常'
+      } else {
+        
+        console.log(that.type)
+        item.caution = '低'
+      }
+      if(item.nrv === 'undefined%' || item.nrv === ' ') {
+        item.nrv = '0%'
+      }
+      this.setData({
+        result: temp
+      })
     })
   }
 })
--- pages/knowledge_detail/knowledge_detail.js
+++ pages/knowledge_detail/knowledge_detail.js
@@ -1,6 +1,5 @@
-// pages/knowledge_detail/knowledge_detail.js
+const db = wx.cloud.database()
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -224,60 +223,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    db.collection('news_list').get().then(res => {
+      console.log(res)
+    })
+
+    console.log(options)
     // 接收id
     let articleId = options.id
     console.log(articleId)
     this.setData({
       articles: this.data.articles[articleId]
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
